9d26f8f522688b8127849fe5602ec113b9353e05
lizhixin3016 <lizhixin3016@foxmail.com>
2015-03-19 11:49:49 +0800

    decond

--- src/com/example/duitangtest/MainActivity.java
+++ src/com/example/duitangtest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.duitangtest;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -10,15 +12,19 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	ImageView iv1,iv2,iv3,iv4,iv5,iv6;
 	Bitmap bm1 = null ,bm2 = null, bm3 = null ,bm4 = null ,bm5 = null ,bm6 = null;
+	boolean b1,b2, b3,b4,b5,b6;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +36,22 @@ public class MainActivity extends Activity {
 		iv4 = (ImageView) findViewById(R.id.imageView4);
 		iv5 = (ImageView) findViewById(R.id.imageView5);
 		iv6 = (ImageView) findViewById(R.id.imageView6);
-		
+		b1 = b2 = b3 = b4 = b5 = b6 = false;
 		
 		new Thread(){
 			public void run(){
 				
 				bm1 = getbitmap("http://cdnq.duitang.com/uploads/item/201410/14/20141014185925_FBQNS.thumb.700_0.jpeg");
 				bm2 = getbitmap("http://cdnq.duitang.com/uploads/item/201409/30/20140930170248_GsFv4.thumb.700_0.jpeg");
-
-						
+				b1 = b2 = true;		
                 Log.v("SCAN","Thread2");
                 iv1.post(new Runnable() {  
-                            @Override  
-                            public void run() { 
-                            	iv1.setImageBitmap(bm1);
-                            	iv2.setImageBitmap(bm2);
-                            }
-                        });
+                    @Override  
+                    public void run() { 
+                    	iv1.setImageBitmap(bm1);
+                    	iv2.setImageBitmap(bm2);
+                    }
+                });
 				}
 		}.start();
 		
@@ -54,16 +59,17 @@ public class MainActivity extends Activity {
 			public void run(){
 				bm3 = getbitmap("http://img5q.duitang.com/uploads/item/201408/13/20140813190048_jRSdB.thumb.700_0.png");
 				bm4 = getbitmap("http://img5q.duitang.com/uploads/item/201411/06/20141106161920_MMNVv.thumb.700_0.jpeg");
-						
+				b3 = b4 = true ;
+				
                 Log.v("SCAN","Thread2");
                 iv3.post(new Runnable() {  
-                            @Override  
-                            public void run() {
-                            	iv3.setImageBitmap(bm3);
-                            	iv4.setImageBitmap(bm4);
-                            	
-                            }
-                        });
+                    @Override  
+                    public void run() {
+                    	iv3.setImageBitmap(bm3);
+                    	iv4.setImageBitmap(bm4);
+                    	
+                    }
+                   });
 				}
 		}.start();
 		
@@ -71,20 +77,113 @@ public class MainActivity extends Activity {
 			public void run(){
 				bm5 = getbitmap("http://img5q.duitang.com/uploads/item/201407/03/20140703170553_wsF5a.thumb.700_0.png");
 				bm6 = getbitmap("http://cdnq.duitang.com/uploads/blog/201411/03/20141103203721_mXhuR.thumb.700_0.gif");
-						
+					
+				b5 = b6 = true;
                 Log.v("SCAN","Thread2");
                 iv5.post(new Runnable() {  
-                            @Override  
-                            public void run() { 
-                            	iv5.setImageBitmap(bm5);
-                            	iv6.setImageBitmap(bm6);
-                            }
-                        });
+                    @Override  
+                    public void run() { 
+                    	iv5.setImageBitmap(bm5);
+                    	iv6.setImageBitmap(bm6);
+                    }
+                  });
 				}
 		}.start();
 		
+		iv1.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if(b1 == false ) return ;
+				SaveImage(bm1,"pic2.jpg");
+			}
+		}
+		);
+		
+		iv2.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if(b2 == false ) return ;
+				SaveImage(bm2,"pic1.jpg");
+			}
+		}
+		);
+		
+		iv3.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if(b3 == false ) return ;
+				SaveImage(bm3,"pic3.jpg");
+			}
+		}
+		);
+		
+		iv4.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if(b4 == false ) return ;
+				SaveImage(bm4,"pic4.jpg");
+			}
+		}
+		);
+		
+		iv5.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if(b5 == false ) return ;
+				SaveImage(bm5,"pic5.jpg");
+			}
+		}
+		);
+		
+		iv6.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if(b6 == false ) return ;
+				SaveImage(bm6,"pic6.gif");
+			}
+		}
+		);
+		
+		
+		
 	}
 
+	private boolean SaveImage(Bitmap bm,String name) {
+		if(bm == null) return false;
+		else{
+			String filepath = Environment.getExternalStorageDirectory() + "/DuitangTest_" + name ;
+			File file= new File(filepath);
+			try {
+				file.createNewFile();
+				FileOutputStream FileS = new FileOutputStream(file,false);
+				bm.compress(Bitmap.CompressFormat.PNG, 60, FileS);
+				FileS.flush();              
+	    		FileS.close(); 
+	    		
+	    		Log.d("LOG","debug10");
+	    		
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				Toast.makeText(this, "存储文件失败", Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+				Log.e("LOG","error10");
+				return false;
+			}
+			
+			Toast.makeText(this, "文件已存储到：" + filepath, Toast.LENGTH_LONG).show();
+			Log.d("LOG","debug11");
+			return true;
+		}
+		// TODO Auto-generated method stub
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
